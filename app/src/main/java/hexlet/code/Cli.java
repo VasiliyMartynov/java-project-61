c96c5382707e697c314e00c11b625cbe8cd805ba
Vasiliy Martynov <vmartynov@outlook.com>
2022-11-16 22:24:49 +0300

    ADD CLI.java, Makefile MOD App.java, build.gradle

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+final class Cli {
+    public static void gretings(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = sc.next();
+        System.out.println("Hello " + userName + "!");
+    }
+
+}
