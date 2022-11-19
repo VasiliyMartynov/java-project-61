3d73a22680d5db19e170bd07b94ffdcd955075e6
Vasiliy Martynov <vmartynov@outlook.com>
2022-11-19 12:12:15 +0300

    MOD CLI.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,7 +3,7 @@ package hexlet.code;
 import java.util.Scanner;
 
 final class Cli {
-    public static void gretings(){
+    public static void gretings() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
