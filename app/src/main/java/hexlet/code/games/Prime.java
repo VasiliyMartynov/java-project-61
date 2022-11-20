ff6072c8fde6fa63c28bb14a1451f2ea1e0ec5dd
Vasiliy Martynov <vmartynov@outlook.com>
2022-11-20 18:56:32 +0300

    MOD Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -4,7 +4,7 @@ public class Prime {
     public static int[] primeNumbers = {
         2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
     public static void rules() {
-        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'");
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
     }
     public static String[][] setGameData() { //[0] - question, [length-1] - answer
         String[][] gameData = new String[Engine.NUMBERSOFGAMES][2];
